a8280d3022703ee3def1af2049ea2f1009392ccd
Anthony Ardron <anthonyardron@Administrators-MacBook-Pro.local>
2018-11-27 15:58:34 +0000

    Added terraformer files and refactored structure

--- Management/Infrastructure/management_vpc/terraform.tf
+++ Management/Infrastructure/management_vpc/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
     bucket = "my-booky-wooky"
-    key    = "vpc/terraform.tfstate"
+    key    = "Management/Infrastruture/management_vpc/terraform.tfstate"
     region = "eu-west-1"
     }
 }
