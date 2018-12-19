4f0aaa277031c191d7420fbeca527406b7eae9f1
Anthony Ardron <anthonyardron@Administrators-MacBook-Pro.local>
2018-12-19 13:45:50 +0000

    Removed redundant code

--- Modules/Service/Bastion/aws_security_group_rules.tf
+++ Modules/Service/Bastion/aws_security_group_rules.tf
@@ -16,23 +16,23 @@ resource "aws_security_group_rule" "link_security_group_1_out" {
   security_group_id = "${aws_security_group.instance.id}"
 }
 
-resource "aws_security_group_rule" "link_security_group_2" {
-  type            = "ingress"
-  from_port       = 443
-  to_port         = 443
-  protocol        = "tcp"
-  cidr_blocks = ["${var.my_ip}"]
-  security_group_id = "${aws_security_group.bastion.id}"
-}
+# resource "aws_security_group_rule" "link_security_group_2" {
+#   type            = "ingress"
+#   from_port       = 443
+#   to_port         = 443
+#   protocol        = "tcp"
+#   cidr_blocks = ["${var.my_ip}"]
+#   security_group_id = "${aws_security_group.bastion.id}"
+# }
 
-resource "aws_security_group_rule" "link_security_group_2_out" {
-  type            = "egress"
-  from_port       = 443
-  to_port         = 443
-  protocol        = "tcp"
-  cidr_blocks = ["${var.my_ip}"]
-  security_group_id = "${aws_security_group.bastion.id}"
-}
+# resource "aws_security_group_rule" "link_security_group_2_out" {
+#   type            = "egress"
+#   from_port       = 443
+#   to_port         = 443
+#   protocol        = "tcp"
+#   cidr_blocks = ["${var.my_ip}"]
+#   security_group_id = "${aws_security_group.bastion.id}"
+# }
 
 resource "aws_security_group_rule" "link_security_group_3" {
   type            = "ingress"
